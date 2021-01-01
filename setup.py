063e12efb232c626bbbffbe51e4c889810c5a269
letsplaywithstrings <32226861+letsplaywithstrings@users.noreply.github.com>
2021-01-01 22:27:50 +0300

    Update setup.py

--- setup.py
+++ setup.py
@@ -1,6 +1,6 @@
 from distutils.core import setup
 import setuptools
-VERSION = "1.0.1"
+VERSION = "1.0.0"
 long_description = ""
 with open("README.md", "r", encoding="utf-8") as file:
   long_description = file.read()
